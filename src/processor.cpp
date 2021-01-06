3048f50ab2cf2ba5f25d1047277fd524f0b18bfd
ndzda <ndzda>
2021-01-06 20:35:42 +0800

    ud

--- src/processor.cpp
+++ src/processor.cpp
@@ -10,10 +10,21 @@ namespace nda
     {
         map<string, int> globalVariable; // global variable 全局变量
         map<string, int> constantTable;  // constant table 常量表
+        map<string, int> functionTable;  // function table 函数表
         map<string, int> localVariable;  // Local variable 局部变量
+        int aConstantCount;              // Anonymous constant count 匿名常量计数
+        int aFunctionCount;              // Anonymous function count 匿名函数计数
+        map<string, int> ReferenceTable; // File reference table 文件引用表
     public:
         NASProcessor()
         {
+            globalVariable = map<string, int>();
+            constantTable = map<string, int>();
+            functionTable = map<string, int>();
+            localVariable = map<string, int>();
+            aConstantCount = 0;
+            aFunctionCount = 0;
+            ReferenceTable = map<string, int>();
         }
     };
 } // namespace nda
